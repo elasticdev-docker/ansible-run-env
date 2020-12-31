40f7ce351848ff608e9fb99b537dc39685b0cabb
Gary <gary@elasticdev.io>
2020-12-31 23:58:50 +0800

    update

--- run.sh
+++ run.sh
@@ -5,6 +5,13 @@ export EXEC_DIR=${EXEC_DIR:=/var/tmp/share/var/tmp/ansible}
 
 cd $EXEC_DIR || exit 4
 
+echo ""
+echo ""
+echo "the configuration files in exec_dir $EXEC_DIR"
+find .
+echo ""
+echo ""
+
 for i in ${ANSIBLE_EXEC_YMLS//,/ }
 do
    # call your procedure/other scripts here below
